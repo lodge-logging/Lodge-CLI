5c61d2aeac3f94c68500263719a5ac257c2fc94e
ranadeeb92 <62123706+ranadeeb92@users.noreply.github.com>
2021-12-14 15:04:17 -0800

    updated welcome.js and added colors
    Co-authored-by: SamClark <63471713+clarksam19@users.noreply.github.com>
    Co-authored-by: ReginaDonovan <55225918+rgdonovan@users.noreply.github.com>
    Co-authored-by: JustinLo <50888159+justinnnlo@users.noreply.github.com>

--- src/init.js
+++ src/init.js
@@ -1,19 +1,28 @@
 const sh = require("shelljs");
+const art = require("ascii-art");
 const { execSync } = require("child_process");
 const { writeFileSync } = require("fs");
-const { checkGlobalInstall } = require('../lib/aws');
-const { APP_NAME, COMMANDS, KEY_NAME, KEY_PATH, REPO } = require('../lib/constants');
+const { checkGlobalInstall } = require("../lib/aws");
+const {
+  APP_NAME,
+  COMMANDS,
+  KEY_NAME,
+  KEY_PATH,
+  REPO,
+} = require("../lib/constants");
 
-const options = {stdio : 'pipe' };
+const options = { stdio: "pipe" };
 
 async function generateKey() {
   let res;
   try {
-    console.log('Generating SSH key...');
+    console.log("Generating SSH key...");
     res = execSync(`${COMMANDS.CREATE_KEY} ${KEY_NAME}`, options);
-    console.log('Done');
+    console.log(art.style("Done", "green"));
   } catch (error) {
-    console.log('Failed to create SSH key. Please see error below:')
+    console.log(
+      art.style("Failed to create SSH key. Please see error below:", "red")
+    );
     console.log(error);
     process.exit();
   }
@@ -23,7 +32,9 @@ async function generateKey() {
     console.log(`Writing SSH key to ${KEY_PATH}...`);
     writeFileSync(KEY_PATH, key);
   } catch (error) {
-    console.error('Failed to write key to file. Please see error below:');
+    console.error(
+      art.style("Failed to write key to file. Please see error below:", "red")
+    );
     console.error(error);
     process.exit();
   }
@@ -31,11 +42,11 @@ async function generateKey() {
 
 async function cloneAndInstall(repo) {
   try {
-    console.log('Cloning CDK app into lodge-app directory...')
+    console.log("Cloning CDK app into lodge-app directory...");
     execSync(`git clone ${repo} ${APP_NAME}`, options);
-    console.log('Done');
+    console.log(art.style("Done", "green"));
   } catch (error) {
-    console.error('Clone failed. Please see error below: ');
+    console.error(art.style("Clone failed. Please see error below: ", "red"));
     console.error(error);
     process.exit();
   }
@@ -43,11 +54,16 @@ async function cloneAndInstall(repo) {
   sh.cd(APP_NAME);
 
   try {
-    console.log('Installing CDK app dependencies...')
+    console.log("Installing CDK app dependencies...");
     execSync("npm install", options);
-    console.log('Done');
+    console.log(art.style("Done", "green"));
   } catch (error) {
-    console.error('Failed to install dependencies. Please see error below:');
+    console.error(
+      art.style(
+        "Failed to install dependencies. Please see error below:",
+        "red"
+      )
+    );
     console.error(error);
     process.exit();
   }
@@ -58,31 +74,42 @@ async function checkRegion() {
     const res = execSync(COMMANDS.GET_AZS);
     const azList = JSON.parse(res).AvailabilityZones;
     if (azList.length < 3) {
-      console.error(`AWS Region ${azList[0].RegionName} has less than 3 availability zones. Please use a different region`);
+      console.error(
+        `AWS Region ${azList[0].RegionName} has less than 3 availability zones. Please use a different region`
+      );
       process.exit();
     }
   } catch (error) {
-    console.error('Failed to fetch availability zones from your region. Is your AWS CLI configured?');
+    console.error(
+      art.style(
+        "Failed to fetch availability zones from your region. Is your AWS CLI configured?",
+        "red"
+      )
+    );
     console.error(error);
   }
 }
 
 module.exports = async function init(args) {
   try {
-    console.log('Verifying global installation of AWS CLI, AWS CDK, and AWS SSM...');
-    await checkGlobalInstall(['aws', 'cdk', 'aws ssm']);
-    console.log('Verified');
-    console.log('Verifying 3 availability zones in your region...');
+    console.log(
+      "Verifying global installation of AWS CLI, AWS CDK, and AWS SSM..."
+    );
+    await checkGlobalInstall(["aws", "cdk", "aws ssm"]);
+    console.log(art.style("Verified", "green"));
+    console.log("Verifying 3 availability zones in your region...");
     await checkRegion();
-    console.log('Verified');
+    console.log(art.style("Verified", "green"));
   } catch (error) {
     console.error(error);
   }
   try {
     await cloneAndInstall(REPO);
     await generateKey();
-    console.log('Lodge initialization complete. Run "lodge deploy" to deploy Lodge');
+    console.log(
+      'Lodge initialization complete. Run "lodge deploy" to deploy Lodge'
+    );
   } catch (error) {
     console.error(error);
   }
-}
+};
